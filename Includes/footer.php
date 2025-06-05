b136e96e183bf42560f9765fde59541362f53bdc
charafsy <charafdieny@gmail.com>
2025-06-05 09:26:04 +0200

    change footer and develop the contact page

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,12 +1,16 @@
 </main>
-<footer class="main-footer bg-primary text-white text-center py-3 mt-5">
+
+<footer class="bg-primary text-white text-center py-3 mt-5">
     <div class="container">
         <p class="mb-0">&copy; <?php echo date("Y"); ?> Talent Show | All rights reserved.</p>
     </div>
 </footer>
-<!-- Bootstrap JS Bundle -->
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<!-- Custom JS -->
-<script src="/Web/Talentenshow-project/assets/js/script.js"></script>
+
+
+<script src="assets/js/script.js"></script>
+
 </body>
 </html>
